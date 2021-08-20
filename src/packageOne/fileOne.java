c27e80439a4c1d232a58f462f795197f84ce78ce
leninyenni <66212934+leninyenni@users.noreply.github.com>
2021-08-21 00:03:31 +0530

    Update fileOne.java

--- src/packageOne/fileOne.java
+++ src/packageOne/fileOne.java
@@ -3,5 +3,6 @@ package packageOne;
 public class fileOne {
     public static void main(String[] args) {
         System.out.println("first line local master");
+        System.out.println("second line local master");
     }
 }
