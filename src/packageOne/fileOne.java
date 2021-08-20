29b7bf704a951a959db6b745d02266edd339a3cb
leninyenni <66212934+leninyenni@users.noreply.github.com>
2021-08-21 00:31:38 +0530

    Update fileOne.java

--- src/packageOne/fileOne.java
+++ src/packageOne/fileOne.java
@@ -2,9 +2,6 @@ package packageOne;
 
 public class fileOne {
     public static void main(String[] args) {
-        System.out.println("first line local master");
-        System.out.println("adding code with out committing");
-        System.out.println("adding code with out committing again");
-        System.out.println("adding again with committing");
+        System.out.println("first line changed from master");
     }
 }
